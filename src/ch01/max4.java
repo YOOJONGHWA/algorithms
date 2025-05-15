b1890c6d67aaac0878ecf0877c25aff93c1ac7df
유종화 <sola201@naver.com>
2025-05-15 22:09:40 +0900

    1장 기본 알고리즘 연습

---
+++ src/ch01/max4.java
@@ -0,0 +1,61 @@
+package ch01;
+
+public class max4 {
+
+    static int max4(int a, int b, int c, int d) {
+
+        int max = a;
+
+        if (max < b) {
+            max = b;
+        }
+
+        if (max < c) {
+            max = c;
+        }
+
+        if (max < d) {
+            max = d;
+        }
+
+        return max;
+    }
+
+    static int min3(int a, int b, int c) {
+        int min = a;
+
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > c) {
+            min = c;
+        }
+
+        return min;
+    }
+
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (min > b) {
+            min = b;
+        }
+
+        if (min > c) {
+            min = c;
+        }
+
+        if (min > d) {
+            min = d;
+        }
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("max4(1 , 2 , 3 ,4)" + " = " +  max4(1, 2, 3, 4));
+        System.out.println("min3(1 , 2 , 3)" + " = " +  min3(1, 2, 3));
+        System.out.println("min4(1 , 2 , 3, 4)" + " = " + min4(1, 2, 3, 4));
+    }
+}
